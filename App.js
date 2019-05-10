abad8efa87280b8ef17147d24041e6251a21f388
anil123apalya <anilkumar.r@apalya.com>
2019-05-10 23:28:22 +0530

    removed pods

--- App.js
+++ App.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, requireNativeComponent, Image } from 'react-native';
-// import Video from 'react-native-video';
 import Video from './src/components/KVCVideo';
 export default class App extends Component {
   render() {
-    const HelloWorldSquare = requireNativeComponent('HelloWorldSquare');
     const playBtnImg = require('./src/assets/img/player_play_button.png');
     const pauseBtnImg = require('./src/assets/img/player_pause_button.png');
     return <React.Fragment>
@@ -18,22 +15,3 @@ export default class App extends Component {
     </React.Fragment>
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
